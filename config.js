ba9731485c8d6c10e6c0caa9e23e3ec4704774e0
luizfernandomartins <luiz.theolem.martins@escola.pr.gov.br>
2026-07-14 16:08:18 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+/*
+  CONFIGURAÇÃO DO PREPARATÓRIO OLITEF
+  Troque somente os textos entre aspas.
+*/
+window.OLITEF_CONFIG = {
+  teacherEmail: "luiz.theolem.martins@escola.pr.gov.br",
+
+  firebase: {
+    apiKey: "AIzaSyC_cAt5o6FoRWoLPqStP73Ks6iT69kHojE",
+    databaseURL: "https://olitef1-default-rtdb.firebaseio.com/"
+  },
+
+  emailjs: {
+    publicKey: "Q5xC9vRB87HVGIJnk",
+    serviceId: "service_27w2v39",
+    templateId: "template_5k54d8c"
+  }
+};
